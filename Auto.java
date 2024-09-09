389155927c34d6650a15a42bd05a8ef308099297
Giumbociccio <tommaso.corvini@liceovittoriacolonnaroma.edu.it>
2024-09-09 11:39:58 +0200

    Delete Auto.java

--- Auto.java
+++
@@ -1,8 +0,0 @@
-public class Auto<A>{
-  private A alimentazione;
-  private int cilindrata;
-  public Auto(A alimentazione, int cilindrata){
-    this.alimentazione = alimentazione;
-    this.cilindrata = cilindrata;
-  }
-}
